f7f250d005d181f19c2ee6db5dd5ab4d204ef50a
laksh <lakshyam38@gmail.com>
2021-10-13 19:35:47 +0530

    prototype player UI done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="title">
+        <p>Visiting Hours</p>
+      </div>
+      <div className="artist">
+        <p>ED SHEERAN</p>
+      </div>
+      <div className="cover">
+        <img
+          src="https://i.scdn.co/image/ab67616d0000b273a5cf426490a6e1c1bde53fb1"
+          alt=""
+        />
+      </div>
+      <div className="player">
+        <audio
+          src="https://p.scdn.co/mp3-preview/a26c21c3454638cd22c40faf44d6e68b58bcfef8?cid=774b29d4f13844c495f206cafdad9c86"
+          autoplay
+        ></audio>
+      </div>
+      <button className="spotify">Open in Spotify</button>
+      <button className="shuffle">Random</button>
     </div>
   );
 }
