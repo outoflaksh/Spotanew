85efb994f1e50ac349d77ef1a788894ef3b222fd
laksh <lakshyam38@gmail.com>
2022-02-01 17:24:27 +0530

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Song from "./components/Song";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="page-wrapper">
       <Header />
       <Song />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        Created by{" "}
+        <a href="https://github.com/outoflaksh" target="_blank">
+          Lakshya Malik
+        </a>
+      </p>
+      <p>
+        <a href="https://github.com/outoflaksh/Spotanew" target="_blank">
+          View on GitHub
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
